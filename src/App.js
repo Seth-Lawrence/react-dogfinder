98caf3561def8b64dabccf5641ed18899d1a2743
Nicholas <nick.orsi1992@gmail.com>
2024-01-22 17:03:43 -0800

    Added some pseudo code to App.js and some component files.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
-import RouteList from './RouteList'
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+
+/**
+ * App component makes AJAX request to get list of dogs and uses this to pass as
+ * a prop to the child components as needed. Renders NavBar and RoutesList.
+ *
+ * Props:
+ * -None
+ * State:
+ * -dataFetch - Tracks whether AJAZ request complete or not
+ *
+ * App -> {NavBar, RoutesList}
+ */
+
+async function App() {
+  const [dataFetch, setDataFetch] = useState();
+
+  //AJAX request for dog list
+  //Asynchronous? How to delay this so it doesn't auto fail when Promise first
+  //returned?
+
+  const response = await fetch("http://localhost:5001/dogs");
+  const dogs = await response.json();
+
   return (
     <div className="App">
-      <h1>Good luck!</h1>
+      <Navbar dogs={dogs}/>
       <RouteList />
     </div>
   );
